150762b86e2c4aadd7fb46d5bccce4fd1d1580a4
Austin <austinmasibo@gmail.com>
2014-05-31 19:32:46 +0300

    some cleverer comments

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ chesscompgns
 ============
 
 An esoteric python script to download all chess.com pgn files in gmail.
+For people that use their blitz games for learning purposes,this can be useful to create a database of chess.com games and see what openings are working.
+
+It's works in python 2.7
+Most of the work had been done here:
+
+http://stackoverflow.com/questions/10182499/how-do-i-download-only-unread-attachments-from-a-specific-gmail-label?rq=1
--- chessattachments.py
+++ chessattachments.py
@@ -1,20 +1,30 @@
-#first we get the libraries that will be needed
+#get the libraries that will be needed.imaplib implements a client for the gmail IMAP4 servers.
 import email,getpass,imaplib,os
 
-#folder where the attachments will be downloaded to 	
+#folder where the attachments will be downloaded to.Defaults to the current folder.
 detach_dir = "."
 
+#get the user account details
+hostname = "imap.gmail.com"
 username = raw_input("Please enter your gmail username : ")
 password = getpass.getpass("Please enter your gmail password: ")
 
-m = imaplib.IMAP4_SSL("imap.gmail.com")
+#connect to the gmail imap server
+m = imaplib.IMAP4_SSL(hostname)
 m.login(username,password)
 
-
+#Select a mailbox to act on e.g "INBOX".Returned data is the count of messages in mailbox.This selects all mail(duh).
+#You can use m.list to view all other mailboxes.
 m.select("[Gmail]/All Mail")
+
+#Search the mailbox for messages matching the criteria.Returns count of messages as well.
+#To view various imap search criteria check here:	http://www.example-code.com/csharp/imap-search-critera.asp
 resp,items = m.search(None,'FROM','"Chess.com"')
+#*********What does m.search return?
+#*********split function
 items = items[0].split()
 for emailid in items:
+	#The RFC822 means get everything in the mail.Headers/Content/Attachments etc
 	resp,data = m.fetch(emailid,"(RFC822)")
 	email_body = data[0][1]
 	mail = email.message_from_string(email_body)
